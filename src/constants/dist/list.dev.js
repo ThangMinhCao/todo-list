cce9649cb46e6c757935df50d0fc15c3feb9fbba
ThangMinhCao <thangminhcao@gmail.com>
2020-08-27 22:19:24 -0400

    Added lists on ListPanel component

---
+++ src/constants/dist/list.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var ListTypes = {
+  TODO: 1,
+  REMINDERS: 2,
+  SCHEDULE: 3
+};
+var _default = ListTypes;
+exports["default"] = _default;
---
+++ src/constants/list.js
@@ -0,0 +1,7 @@
+const ListTypes = {
+  TODO: 1,
+  REMINDERS: 2,
+  SCHEDULE: 3,
+};
+
+export default ListTypes;
