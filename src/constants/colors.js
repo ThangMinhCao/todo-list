b3b56f65a060c660ef333784329f1c0d754dc373
ThangMinhCao <thangminhcao@gmail.com>
2020-08-28 18:06:28 -0400

    Built the structure for the Todo/Reminders/Schedule feature

---
+++ src/constants/colors.js
@@ -0,0 +1,16 @@
+const Colors = {
+  todo: '#115293',
+  todoHover: '#d1d9ff',
+  todoSelected: '#aab6fe',
+
+  reminders: '#d32f2f',
+  reminderHover: '#ffcccb',
+  remindersSelected: '#ffa4a2',
+
+  schedule: '#388e3c',
+  scheduleHover: '#d7ffd9',
+  scheduleSelected: '#b2fab4',
+
+};
+
+export default Colors;
---
+++ src/constants/dist/colors.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var Colors = {
+  todo: '#115293',
+  todoHover: '#d1d9ff',
+  todoSelected: '#aab6fe',
+  reminders: '#d32f2f',
+  reminderHover: '#ffcccb',
+  remindersSelected: '#ffa4a2',
+  schedule: '#388e3c',
+  scheduleHover: '#d7ffd9',
+  scheduleSelected: '#b2fab4'
+};
+var _default = Colors;
+exports["default"] = _default;
